021fece3e2de63286f8dfb28b74485e2c5a69667
sebastian23p <sebastianp235@outlook.es>
2024-06-28 13:52:16 -0500

    eliminacion de comentarios

--- nestjs-picoyplaca/src/picoyplaca/picoyplaca.service.ts
+++ nestjs-picoyplaca/src/picoyplaca/picoyplaca.service.ts
@@ -15,8 +15,8 @@ export class PicoyplacaService {
     const tiempoNoCircular = (hora: string): boolean => {
       const [horas, min] = hora.split(':').map(Number);
       const horaEnMin = horas * 60 + min;
-      const resultado = (horaEnMin >= 360 && horaEnMin <= 570) || // 06:00 to 09:30
-                        (horaEnMin >= 960 && horaEnMin <= 1260); // 16:00 to 21:00
+      const resultado = (horaEnMin >= 360 && horaEnMin <= 570) ||
+                        (horaEnMin >= 960 && horaEnMin <= 1260); 
       return resultado;
     };
 
@@ -24,20 +24,17 @@ export class PicoyplacaService {
     const UltimoDigito = getUltimoDigito(placa);
     const DiaDeSemana = getDiaDeSemana(fechaObj);
 
-    //console.log(`UltimoDigito: ${UltimoDigito}, DiaDeSemana: ${DiaDeSemana}`);
-
     const nocircula: { [key: number ]: number[] } = {
-      0: [1, 2], // lunes
-      1: [3, 4], // martes
-      2: [5, 6], // miércoles
-      3: [7, 8], // jueves
-      4: [9, 0], // viernes
-      5: [], // sábado
-      6: [] // domingo
+      0: [1, 2], 
+      1: [3, 4], 
+      2: [5, 6], 
+      3: [7, 8], 
+      4: [9, 0], 
+      5: [], 
+      6: [] 
     };
 
     if (DiaDeSemana > 5) {
-      // Domingo y Sábado, sin restricciones
       return true;
     }
 
